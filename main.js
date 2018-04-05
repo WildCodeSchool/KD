daf5b7aeeb8841bec9c562196f6acb172cdc50c6
Cynnah <massot.dorian@gmail.com>
2018-04-06 00:38:47 +0200

    add two buttons and beguin of combat

--- main.js
+++ main.js
@@ -22,18 +22,36 @@ fetch('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
             const sayHello = () => {
                 const output = document.getElementById("output")
                 console.log(output)
+                output.innerHTML = `Vous infligez ${bane.powerstats.strenght} à `
                 //document represente le document affiché par le navigateur   
                 }
+                
+                //Affichage des stats
 
                 output.innerHTML = `Hello I'm ${bane.name}`
+
+                output.innerHTML = `<li> Health Points : ${bane.powerstats.combat} </li>
+                                    <li> Strength : ${bane.powerstats.strength} </li>
+                                    <li> Speed : ${bane.powerstats.speed} </li>
+                                    <li> Power : ${bane.powerstats.power} </li>`
               
               const button = document.getElementById("button")
               button.addEventListener('click', sayHello)
+
+            const fuite = () => {
+                const output = document.getElementById("escape")
+                //console.log(escape)
+                output.innerHTML = `Vous avez pris la fuite comme un vilain`
+            }
+
+              const buttonBlue = document.getElementById("buttonBlue")
+              buttonBlue.addEventListener('click', fuite)
 
             // afficher les stats dans un tableau
 
             let list = ""
             console.log(bane.powerstats)
+
             const htmlList = document.getElementById('display')
             htmlList.innerHTML = list
         })
